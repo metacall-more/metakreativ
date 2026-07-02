0f544f44f72640ba98b74197a3bfc9017d70947a
metacall-more <info@metacallandmore.com>
2026-07-02 17:53:04 +0200

    under constrution

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,43 @@ import Services from './components/Services';
 import Portfolio from './components/Portfolio';
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
+import {useState, useEffect} from 'react';
 
 function App() {
-  console.log('test');
+   const [dots, setDots] = useState(".");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prev) => (prev.length >= 3 ? "." : prev + "."));
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <>
-      <Hero />
-      <Clients />
-      <About />
-      <Marquee />
-      <Services />
-      <Portfolio />
-      <Testimonials />
-      <Footer />
-    </>
+    <div className="container">
+      <div className="card">
+        <div className="icon">🚧</div>
+
+        <h1>Under Construction{dots}</h1>
+
+        <div className="loader"></div>
+
+        <div className="footer">
+          © {new Date().getFullYear()} Your Project
+        </div>
+      </div>
+    </div>
+    // <>
+    //   <Hero />
+    //   <Clients />
+    //   <About />
+    //   <Marquee />
+    //   <Services />
+    //   <Portfolio />
+    //   <Testimonials />
+    //   <Footer />
+    // </>
   );
 }
 
