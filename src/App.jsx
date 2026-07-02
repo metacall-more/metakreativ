b139c9f52617cb923a4dc6162530483805238507
Cripxo <cripxo20@gmail.com>
2026-07-02 16:15:06 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
 
 function App() {
-  console.log('App component rendered');
   return (
     <>
       <Hero />
